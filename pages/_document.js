18330abd7299b4c846a323d9f85e7ac09a9cd34b
ruchitagodse <158310655+ruchitagodse@users.noreply.github.com>
2025-05-29 16:25:40 +0530

    Add files via upload

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="icon-192x192.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
